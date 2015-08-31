00626c5545eaed8bebe040974576dbb899fe7ac0
Karsten Gresch <git-2015@by-doing.org>
2015-08-31 17:53:41 +0200

    Decoupling networking

---
+++ stormy/Stormy/Stormy/NetworkOperation.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkOperation.swift
+//  Stormy
+//
+//  Created by Karsten Gresch on 31.08.15.
+//  Copyright (c) 2015 Closure One. All rights reserved.
+//
+
+import Foundation
+
+class NetworkOperation {
+  
+  var config: NSURLSessionConfiguration
+  var session: NSURLSession
+  
+}
