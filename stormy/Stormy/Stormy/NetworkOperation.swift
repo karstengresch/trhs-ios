430823ee7363d8f815af1ca1c012ba6272450123
Karsten Gresch <git-2015@by-doing.org>
2015-08-31 23:16:31 +0200

    Encapsulating the n/w request, closurefied.

--- stormy/Stormy/Stormy/NetworkOperation.swift
+++ stormy/Stormy/Stormy/NetworkOperation.swift
@@ -10,7 +10,43 @@ import Foundation
 
 class NetworkOperation {
   
-  var config: NSURLSessionConfiguration
-  var session: NSURLSession
+  lazy var config: NSURLSessionConfiguration = NSURLSessionConfiguration.defaultSessionConfiguration()
+  lazy var session: NSURLSession = NSURLSession(configuration: self.config)
+  let queryUrl: NSURL
+  
+  typealias JsonDictionaryCompletion = ([String: AnyObject]? -> Void)
+  
+  init(url: NSURL) {
+    self.queryUrl = url
+  }
+  
+  func downloadJsonFromUrl (completion: JsonDictionaryCompletion) {
+    
+    let request: NSURLRequest = NSURLRequest(URL: queryUrl)
+    
+    let dataTask = session.dataTaskWithRequest(request) {
+      (let data, let response, let error) in
+      
+      if let httpResponse = response as? NSHTTPURLResponse {
+        switch(httpResponse.statusCode)
+        {
+        case 200:
+          let jsonDictionary = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: nil) as? [String: AnyObject]
+        default:
+          println("GET request not successful: \(httpResponse.statusCode))")
+        }
+        
+      } else {
+        println("Error: Not a valid HTTP response.")
+      }
+    }
+
+    dataTask.resume()
+    
+    
+  }
+  
+  
+  
   
 }
