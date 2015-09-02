c2813ebf648dd49a36f5f334bf8be224fefc15f5
Karsten Gresch <git-2015@by-doing.org>
2015-09-02 18:13:33 +0200

    Back to mine

--- stormy/Stormy/Stormy/CurrentWeather.swift
+++ stormy/Stormy/Stormy/CurrentWeather.swift
@@ -30,58 +30,64 @@ struct CurrentWeather {
   let summary: String?
   var icon: UIImage? = UIImage(named: "default.png")
   
-  init(weatherDictionary: [String: AnyObject]) {
+  init(weatherDictionary: [String: AnyObject] ) {
     temperature = weatherDictionary["temperature"] as? Int
-    
-    if let humidityFloat = weatherDictionary["humidity"] as? Double {
-      humidity = Int(humidityFloat * 100)
-    } else {
+    if let humidityFloat = weatherDictionary["humidity"] as? Double
+    {
+     humidity = Int(humidityFloat * 100)
+    } else
+    {
       humidity = nil
     }
     
-    if let precipFloat = weatherDictionary["precipProbability"] as? Double {
+    if let precipFloat = weatherDictionary["precipProbability"] as? Double
+    {
       precipProbability = Int(precipFloat * 100)
-    } else {
+    } else
+    {
       precipProbability = nil
     }
-    
+
     summary = weatherDictionary["summary"] as? String
     
     if let iconString = weatherDictionary["icon"] as? String {
       icon = weatherImageFromIconString(iconString)
     }
+    
+  }
+
+func weatherImageFromIconString(iconString: String) -> UIImage? {
+  var imageName: String
+  
+  if let iconValue = Icon(rawValue: iconString) {
+    switch iconValue {
+    case .ClearDay:
+      imageName = "clear-day.png"
+    case .ClearNight:
+      imageName = "clear-night.png"
+    case .Rain:
+      imageName = "rain.png"
+    case .Snow:
+      imageName = "snow.png"
+    case .Sleet:
+      imageName = "sleet.png"
+    case .Wind:
+      imageName = "wind.png"
+    case .Fog:
+      imageName = "fog.png"
+    case .Cloudy:
+      imageName = "cloudy.png"
+    case .PartlyCloudyDay:
+      imageName = "cloudy-day.png"
+    case .PartlyCloudyNight:
+      imageName = "cloudy-night.png"
+    }
+  } else {
+    imageName = "default.png"
   }
   
-  func weatherImageFromIconString(iconString: String) -> UIImage? {
-    var imageName: String
-    
-    if let iconValue = Icon(rawValue: iconString) {
-      switch iconValue {
-      case .ClearDay:
-        imageName = "clear-day.png"
-      case .ClearNight:
-        imageName = "clear-night.png"
-      case .Rain:
-        imageName = "rain.png"
-      case .Snow:
-        imageName = "snow.png"
-      case .Sleet:
-        imageName = "sleet.png"
-      case .Wind:
-        imageName = "wind.png"
-      case .Fog:
-        imageName = "fog.png"
-      case .Cloudy:
-        imageName = "cloudy.png"
-      case .PartlyCloudyDay:
-        imageName = "cloudy-day.png"
-      case .PartlyCloudyNight:
-        imageName = "cloudy-night.png"
-      }
-    } else {
-      imageName = "default.png"
-    }
-    
-    return UIImage(named: imageName)
-  }
+  return UIImage(named: imageName)
+  
+}
+
 }
