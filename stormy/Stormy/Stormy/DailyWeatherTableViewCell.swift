bf31995f0a6fe2aa237b8d6ca87d1372ea559e7f
Karsten Gresch <git-2015@by-doing.org>
2015-09-10 21:46:55 +0200

    Cell creation.

---
+++ stormy/Stormy/Stormy/DailyWeatherTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DailyWeatherTableViewCell.swift
+//  Stormy
+//
+//  Created by Karsten Gresch on 10.09.15.
+//  Copyright (c) 2015 Closure One. All rights reserved.
+//
+
+import UIKit
+
+class DailyWeatherTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
