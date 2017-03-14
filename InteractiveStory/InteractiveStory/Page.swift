964a99a2e57ace4c862da3351317da6177e9b90d
Karsten Gresch <git-2015@by-doing.org>
2017-03-14 22:02:29 +0100

    finalizing the struct

--- InteractiveStory/InteractiveStory/Page.swift
+++ InteractiveStory/InteractiveStory/Page.swift
@@ -54,6 +54,18 @@ struct Adventure {
     let rower = touchdown.addChoiceWith(title: "Explore the Rover", story: .rover)
     let crate = touchdown.addChoiceWith(title: "Open the Crate", story: .crate)
     
+    homeward.addChoiceWith(title: "Head back to Mars", page: touchdown)
+    let home = homeward.addChoiceWith(title: "Continue Home to Earth", story: .home)
+    
+    let cave = rover.addChoiceWith(title: "Explore the Coordinates", story: .cave)
+    rover.addChoiceWith(title: "Return to Earth", page: home)
+    
+    cave.addChoiceWith(title: "Continue towards faint light", story: .droid)
+    cave.addChoiceWith(title: "Refill the ship and explore the rover", page: rover)
+    
+    crate.addChoiceWith(title: "Explore the Rover", page: rover)
+    crate.addChoiceWith(title: "Use the key", story: .monster)
+    
     return returnTrip
   }
   
