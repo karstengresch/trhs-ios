a901e5ee182edc023e79bda7e10cdeaa527f50e3
Karsten Gresch <git-2015@by-doing.org>
2017-03-16 18:55:29 +0100

    segue method impl

--- InteractiveStory/InteractiveStory/ViewController.swift
+++ InteractiveStory/InteractiveStory/ViewController.swift
@@ -27,7 +27,13 @@ class ViewController: UIViewController {
   
   
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-    
+    if segue.identifier == "startAdventure" {
+      guard let pageController = segue.destination as? PageController else {
+        return
+      }
+      
+      pageController.page = Adventure.story
+    }
   }
 
 
