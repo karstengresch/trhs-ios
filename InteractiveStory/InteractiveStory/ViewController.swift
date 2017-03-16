3562a543453b449ab2683853e6322a8dccb08a69
Karsten Gresch <git-2015@by-doing.org>
2017-03-16 18:50:40 +0100

    Added prepareForSegue

--- InteractiveStory/InteractiveStory/ViewController.swift
+++ InteractiveStory/InteractiveStory/ViewController.swift
@@ -24,6 +24,11 @@ class ViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    
+  }
 
 
 }
