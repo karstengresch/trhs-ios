7767bd3e478588f983e5bddc147b1d3e85baa3c8
Karsten Gresch <git-2015@by-doing.org>
2017-03-27 21:13:14 +0200

    audio/story mangling impl

--- InteractiveStory/InteractiveStory/Audio.swift
+++ InteractiveStory/InteractiveStory/Audio.swift
@@ -10,8 +10,20 @@ import Foundation
 import AudioToolbox
 
 extension Story {
+  
+  var soundEffectName: String {
+    switch self {
+      case .droid, .home: return "HappyEnding"
+      case .monster: return "Ominous"
+      default: return "PageTurn"
+      
+    }
+  }
+  
+  
   var soundEffectUrl: URL {
-    
+    let path = Bundle.main.path(forResource: soundEffectName, ofType: "wav")!
+    return URL(fileURLWithPath: path)
   }
 }
 
